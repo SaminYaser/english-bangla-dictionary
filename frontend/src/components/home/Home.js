cdfb289d322c9a7b0ecfb3bce324b535a4718f2e
A.M. Aahad <45429535+Viper4717@users.noreply.github.com>
2021-02-25 20:36:27 +0600

    Basic homepage created

--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Home.css'
+import { Form, FormControl, Button, Container } from 'react-bootstrap';
+import { CgSearch } from 'react-icons/cg';
 
 function Home() {
+    const [searchText, setSearchText] = useState();
+
+    function parseSearchText(e){
+        setSearchText(categoryParse(e.target.value));
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+    }
+
     return (
-        <div className="Home">
-            The home works
-        </div>
+        <Container fluid="md" className="parentContainer">
+            <div className="headerText">
+                English to Bangla Dictionary
+            </div>
+            <div className="searchBarDiv">
+                <Form className="searchBar" onSubmit={handleSubmit}>
+                    <div style={{display: "flex", flexDirection: "row"}}>
+                        <FormControl type="text" placeholder="Search Words" className="mr-sm-2" 
+                            onChange={parseSearchText}/>
+                        <Button variant="custom" type="submit" size='sm'>
+                        <CgSearch size='2em'/>
+                        </Button>
+                    </div>
+                </Form>
+            </div>
+            <div className="resultDiv">
+                
+            </div>
+        </Container>
     );
   }
   
