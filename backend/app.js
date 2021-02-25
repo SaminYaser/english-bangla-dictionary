f78476ba6b21b2f98e5d70c49a8f5274221351c3
A.M. Aahad <45429535+Viper4717@users.noreply.github.com>
2021-02-25 23:31:49 +0600

    primary and secondary hash function implemented

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,16 @@
 const express = require("express");
-
 const app = express();
+const controller = require("./controller")
 
 app.get('/', (req, res) => {
     res.send("Bravo it worked!");
 });
 
-app.listen(3001);
+app.listen(3001,async ()=>{
+
+    await controller.storeData();
+    await controller.primaryHash();
+    await controller.secondaryHash()
+
+    console.log("Server Started");
+});
---
+++ backend/controller.js
@@ -0,0 +1,60 @@
+const data = require('./BengaliDictionary.json');
+const slot = 16920;
+const p = 652339;
+//const p2 = 652343;
+var primaryHashArray;
+var secondaryHashArray;
+var a = 1+Math.floor(Math.random()*(p-1));
+var b = Math.floor(Math.random()*(p-1));
+
+const primaryHash = async () => {
+    var len = data.length;
+    primaryHashArray = new Array(slot);
+    for(var i=0; i<len; i++){
+        var word = data[i].en;
+        var k = wordToNum(word);
+        var hash = ((a*k+b)%p)%slot;
+        if(!primaryHashArray[hash]){
+            primaryHashArray[hash] = data[i];
+        }
+        else{
+            primaryHashArray[hash].push(data[i]);  //chaining
+        }
+    }
+}
+
+const secondaryHash = async () => {
+    secondaryHashArray = new Array(slot);
+    for(var i=0; i<slot; i++){
+        if(!primaryHashArray[i]){
+            continue;
+        }
+        var len = primaryHashArray[i].length;
+        if(len < 2){  //only one word in the slot
+            secondaryHashArray[i] = primaryHashArray[i];
+            continue;
+        }
+        while(true){
+            var a2 = 1+Math.floor(Math.random()*(p-1));
+            var b2 = Math.floor(Math.random()*(p-1));
+            var count = 0;
+            var m = len*len;
+            secondaryHashArray[i] = new Array(m+3);  //first 3 slots are for storing a2, b2 and m
+            for(var j=0; j<len; j++){
+                var word = primaryHashArray[i][j].en;
+                var k = wordToNum(word);
+                var hash = ((a*k+b)%p)%m;
+                if(!secondaryHashArray[i][hash+3]){  //if the slot is empty (means no clash occurred)
+                    count++;
+                }
+                secondaryHashArray[i][hash+3] = primaryHashArray[i][j];
+            }
+            if(count == len){  //if no clash occurred, store the specific a2, b2 and m
+                secondaryHashArray[i][0] = a2;
+                secondaryHashArray[i][1] = b2;
+                secondaryHashArray[i][2] = m;
+                break;
+            }
+        }
+    }
+}
